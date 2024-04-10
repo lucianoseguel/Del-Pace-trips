a90823fbe16c19bda9d2a2e75ef5f19a09098184
lucianoseguel <lucianitoseguel@gmail.com>
2024-04-10 16:31:04 -0300

    5 in out and bodegas updated

---
+++ scripts/datos.js
@@ -0,0 +1,12 @@
+//Variablwes para todos los documentos
+
+
+
+let title = 'Tours, Visita, Degustaciones';
+
+let telefono ='+54 9 261 209-9589';
+
+
+document.getElementById('telefono').innerHTML = telefono;
+
+  
