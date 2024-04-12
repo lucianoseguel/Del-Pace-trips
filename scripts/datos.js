55ace7bae75c4ee535e5151cbe8635b56a2a4234
lucianoseguel <lucianitoseguel@gmail.com>
2024-04-12 13:09:07 -0300

    5.3 images and city tour upload

--- scripts/datos.js
+++ scripts/datos.js
@@ -2,59 +2,9 @@
 
 
 
-let title = 'Tours, Visita, Degustaciones';
+
 
 let telefono = document.querySelector('#telefono')
 telefono.textContent = '+54 9 261 209-9589';
 
 
-
-function portuguese_bodega(){
-
-    var titulo = document.querySelector('#titulo')
-    titulo.textContent = 'Passeios, Visitas, Degustações'
-
-    var packs = document.querySelector('#packs')
-    packs.textContent = 'Pacotes com almoço'
-
-    var titcenas = document.querySelector('#titcenas')
-    titcenas.textContent = 'Jantares'
-
-    
-};
-
-function portuguese_inicio(){
-
-    var titulo = document.querySelector('#titulo')
-    titulo.textContent = 'TRANSFER PRIVADO EM MENDOZA'
-
-
-    var primertitulo = document.querySelector('#primertitulo')
-    primertitulo.textContent = 'Viagens e passeios privados em Mendoza'
-
-    var objh2 = document.querySelector('#objh2')
-    objh2.textContent = 'Nosso objetivo'
-
-    var objetivo = document.querySelector('#objetivo')
-    objetivo.textContent = 'Da "Del Pace Trips Mendoza" oferecemos os melhores passeios privados pelo província de Mendoza estamos focados em ajudar nossos clientes a ter as melhores experiências em os pontos mais importantes da nossa província'
-
-    var servicios = document.querySelector('#titservicios')
-    servicios.textContent = 'Serviços'
-
-    var box1 = document.querySelector('#box1')
-    box1.textContent = 'Vinícolas e visitas'
-
-    var inicio = document.querySelector('#inicio')
-    inicio.textContent = 'Começars'
-    
-    var contacto = document.querySelector('#contacto')
-    contacto.textContent = 'Contato'
-
-    var servicios = document.querySelector('#servicios')
-    servicios.textContent = 'Serviços'
-
-
-}
-
-
-  
--- scripts/languajes.js
+++ scripts/languajes.js
@@ -1,11 +1,123 @@
-function port() {
-        // Selecciona el elemento con el id "texto"
-    document.body.innerHTML = `
-        <p>Nuevo texto para el párrafo.</p>
-        <div>
-            <h2>Nuevo texto para el encabezado.</h2>
-            <p>Nuevo texto para otro párrafo.</p>
-        </div>
-        <button onclick="restaurarTexto()">Restaurar Texto</button>`;
+//IDIOMAS
+
+/* 
+Español = 0
+Portugues = 1
+Ingles = 2
+*/
+
+
+// script.js
+// Obtener el idioma guardado en localStorage o el idioma del navegador
+const idioma = localStorage.getItem('idioma') || navigator.language.substring(0, 2);
+
+// Cargar las traducciones correspondientes al idioma
+fetch(`translations.json`)
+    .then(response => response.json())
+    .then(translations => {
+        // Aplicar las traducciones en la página
+        Object.keys(translations).forEach(key => {
+            const translation = translations[key];
+            const element = document.querySelector(`[data-translate="${key}"]`);
+            if (element && translation.hasOwnProperty(idioma)) {
+                element.textContent = translation[idioma];
+            }
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function portugeuese(){
+      idioma = 1;
+
+      localStorage.setItem('idioma', 1);
+   
+
+     
+  
 }
+
+
+function portuguese_bodega(){
+
+    var titulo = document.querySelector('#titulo')
+    titulo.textContent = 'Passeios, Visitas, Degustações'
+
+    var packs = document.querySelector('#packs')
+    packs.textContent = 'Pacotes com almoço'
+
+    var titcenas = document.querySelector('#titcenas')
+    titcenas.textContent = 'Jantares'
+
     
+};
+
+function portuguese_inicio(){
+
+    var titulo = document.querySelector('#titulo')
+    titulo.textContent = 'TRANSFER PRIVADO EM MENDOZA'
+
+
+    var primertitulo = document.querySelector('#primertitulo')
+    primertitulo.textContent = 'Viagens e passeios privados em Mendoza'
+
+    var objh2 = document.querySelector('#objh2')
+    objh2.textContent = 'Nosso objetivo'
+
+    var objetivo = document.querySelector('#objetivo')
+    objetivo.textContent = 'Da "Del Pace Trips Mendoza" oferecemos os melhores passeios privados pelo província de Mendoza estamos focados em ajudar nossos clientes a ter as melhores experiências em os pontos mais importantes da nossa província'
+
+    var servicios = document.querySelector('#titservicios')
+    servicios.textContent = 'Serviços'
+
+    var box1 = document.querySelector('#box1')
+    box1.textContent = 'Vinícolas e visitas'
+
+    var inicio = document.querySelector('#inicio')
+    inicio.textContent = 'Começars'
+    
+    var contacto = document.querySelector('#contacto')
+    contacto.textContent = 'Contato'
+
+    var servicios = document.querySelector('#servicios')
+    servicios.textContent = 'Serviços'
+
+
+};
+
+
+idioma = document.getElementById('idioma').textContent = idioma;
+
+
+if (window.location.pathname == '/index.html' && idioma == 1){
+
+    portuguese_inicio();
+    
+    } 
+
+if  (window.location.pathname == '/bodegas.html' && idioma == 1)
+    {
+        portuguese_bodega();
+    }
+
+
+
+*/
