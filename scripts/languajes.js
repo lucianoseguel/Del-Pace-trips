dd564b361fd6be5f9610c64995732a83e84e3284
Luciano Seguel <96950349+lucianoseguel@users.noreply.github.com>
2024-04-18 17:58:27 -0300

    6.5 portuges 80%

--- scripts/languajes.js
+++ scripts/languajes.js
@@ -6,7 +6,11 @@ Portugues = 1
 Ingles = 2
 */
 
+//-----------------------ESPAÑOL
 
+function span() {
+    location.reload();
+}
 
 
 function portuguese_index(){
@@ -42,6 +46,8 @@ function portuguese_index(){
         var servicios = document.querySelector('#servicios')
         servicios.textContent = 'Serviços'
 
+
+
         //BODEGAS
     }}
     
@@ -66,16 +72,224 @@ function portuguese_index(){
     catena.textContent = 'Catena Zapata'
 
      }
-
-
-
-
-
 }
 
 
+function portuguese_citytour(){
+    {
+      
+   
+    var titulo = document.querySelector('#titulo')
+    titulo.textContent = 'City Tours pela cidade'
+
+    var packs = document.querySelector('#packs')
+    packs.textContent = 'Passeio pela cidade de Mendoza'
+
+    var info = document.querySelector('#info')
+    info.textContent = 'Explore os cantos mais fascinantes de Mendoza em nosso emocionante city tour. Desde a icônica Plaza Independencia até o imponente Parque General San Martín, mergulhe na rica cultura, história e beleza natural desta encantadora cidade. Com transporte confortável e guias especializados, garantimos uma experiência inesquecível. Reserve agora e descubra a essência de Mendoza conosco!'
+
+    var incluye = document.querySelector('#incluye')
+    incluye.textContent = 'O que o serviço inclui'
+
+    var item1 = document.querySelector('#item1')
+    item1.textContent = '- Guia turístico'
+
+    var item2 = document.querySelector('#item2')
+    item2.textContent = '- Transporte privado com ar-condicionado'
+
+    var item3 = document.querySelector('#item3')
+    item3.textContent = '- Garrafas de água'
+
+
+     } }
 
 
 
 
+     function portuguese_aventuras(){
+        {
+          
+       
+        var titulo = document.querySelector('#titulo')
+        titulo.textContent = 'Aventuras e montanha'
+    
+        var packs = document.querySelector('#packs')
+        packs.textContent = 'Cordilheira'
+    
+        var item1 = document.querySelector('#item1')
+        item1.textContent = 'Cavalgada em Cacheuta'
+    
+        
+    
+         }
+    }
+    
 
+//---------------------Tempus alba y almuerzo en trapiche
+    function portuguese_pack1(){
+        {
+          
+       
+            var titulo = document.querySelector('#titulo')
+            titulo.textContent = 'Tempus Alba, Almoço na Bodega Trapiche'
+        
+            var packs = document.querySelector('#packs')
+            packs.textContent = 'Tour'
+        
+            var info = document.querySelector('#descripcion')
+            info.textContent = 'Descubra a Experiência Vinícola Completa em Mendoza com nosso exclusivo pacote turístico. Comece o dia com um traslado do seu local de hospedagem para a prestigiada Bodega Tempus Alba, onde você poderá desfrutar de degustações de vinho e explorar o processo de produção. Em seguida, continue até a renomada Bodega Trapiche para desfrutar de um almoço de oito pratos harmonizado com vinhos selecionados, cada um complementando perfeitamente os sabores requintados da comida. Tudo isso com o conforto e a conveniência do nosso transporte privado incluído. Mergulhe na cultura vinícola de Mendoza e viva uma experiência inesquecível repleta de sabores e emoções. Esperamos por você para um dia de deleite para os sentidos!'
+        
+            var incluye = document.querySelector('#incluye')
+            incluye.textContent = 'O que o serviço inclui'
+        
+            var item1 = document.querySelector('#item1')
+            item1.textContent = '- Degustação de vinhos premium'
+        
+            var item2 = document.querySelector('#item2')
+            item2.textContent = '- Transporte privado com ar-condicionado'
+
+            var item3 = document.querySelector('#item3')
+            item3.textContent = '- Visita guiada a 2 excelentes vinícolas'
+
+            var item4 = document.querySelector('#item4')
+            item4.textContent = '- Almoço de 8 etapas com harmonização'
+        
+            var item5 = document.querySelector('#item5')
+            item5.textContent = '- Garrafas de água'
+    
+         }
+    }
+
+
+    
+//---------------------MAGUAY TEMPUS ALBA
+
+    function portuguese_pack2(){
+        {
+          
+       
+            var titulo = document.querySelector('#titulo')
+            titulo.textContent = 'Olivicola Maguay - Vinícola Tempus Alba'
+        
+            var packs = document.querySelector('#packs')
+            packs.textContent = 'Tour'
+        
+            var info = document.querySelector('#descripcion')
+            info.textContent = 'Comece o seu dia com uma visita guiada à Olivicola Maguay, onde você descobrirá os segredos da produção de azeite de oliva e desfrutará de degustações de produtos artesanais. Em seguida, continue a sua experiência enológica com uma visita à prestigiosa Bodega Tempus Alba, onde será recebido com uma experiência gastronômica única: um almoço de oito etapas harmonizado com vinhos selecionados da vinícola. Tudo isso com o conforto do nosso transporte privado incluído. Mergulhe em um dia de sabores e aromas inesquecíveis em Mendoza.'
+
+            var incluye = document.querySelector('#incluye')
+            incluye.textContent = 'O que o serviço inclui'
+        
+            var item1 = document.querySelector('#item1')
+            item1.textContent = '- Degustação de Azeite'
+        
+            var item2 = document.querySelector('#item2')
+            item2.textContent = '- Degustação de vinho'
+
+            var item3 = document.querySelector('#item3')
+            item3.textContent = '- Transporte privado com ar condicionado'
+
+            var item4 = document.querySelector('#item4')
+            item4.textContent = '- Almoço de oito etapas harmonizado'
+
+            var item5 = document.querySelector('#item5')
+            item5.textContent = '- Menu infantil'
+
+            var item6 = document.querySelector('#item6')
+            item6.textContent = '- Opções veganas e celíacas'
+        
+            var item7 = document.querySelector('#item7')
+            item7.textContent = '- Garrafas de água'
+    
+         }
+    }
+
+//-------------------------CATENA ZAPATA    
+
+
+    function portuguese_pack3(){
+        {
+          
+       
+            var titulo = document.querySelector('#titulo')
+            titulo.textContent = 'Experiência Catena Zapata'
+        
+            var packs = document.querySelector('#packs')
+            packs.textContent = 'Tour'
+        
+            var info = document.querySelector('#descripcion')
+            info.textContent = 'Mergulhe em uma experiência vinícola inesquecível na prestigiosa Bodega Catena Zapata. Comece com um passeio pelos vinhedos e instalações da vinícola. Desfrute de uma degustação exclusiva dos vinhos emblemáticos da Catena Zapata, maravilhando-se com a complexidade e a elegância de cada taça.'
+
+            var info = document.querySelector('#descripcion2')
+            info.textContent = 'Em seguida, complete sua experiência com uma refeição gourmet no restaurante da vinícola, onde cada prato é cuidadosamente preparado para realçar os sabores dos vinhos da Catena Zapata. Desde entradas frescas até pratos principais requintados e sobremesas indulgentes, cada mordida é uma celebração da rica tradição culinária de Mendoza. Com transporte privado incluído e a atenção personalizada de nossa equipe, esta experiência na Catena Zapata é uma oportunidade única para desfrutar do melhor que a região tem a oferecer em termos de vinho e gastronomia. Reserve agora e entregue-se a um dia de luxo e sofisticação no coração da região vinícola de Mendoza!'
+
+            var incluye = document.querySelector('#incluye')
+            incluye.textContent = 'O que o serviço inclui'
+        
+            var item1 = document.querySelector('#item1')
+            item1.textContent = '- GRANDE variedade de degustação de vinhos'
+        
+            var item2 = document.querySelector('#item2')
+            item2.textContent = '- Transporte privado com ar condicionado'
+
+            var item3 = document.querySelector('#item3')
+            item3.textContent = '- Menu de degustação'
+
+            var item4 = document.querySelector('#item4')
+            item4.textContent = '- Opções veganas'
+
+            var item5 = document.querySelector('#item5')
+            item5.textContent = '- Garrafas de água'
+/*
+            var item6 = document.querySelector('#item6')
+            item6.textContent = '- Opções veganas e celíacas'
+        
+            var item7 = document.querySelector('#item7')
+            item7.textContent = '- Garrafas de água'
+    */
+         }
+    }
+
+
+
+    //-------------CENA TRAPICHE ULTRA PREMIUM
+
+    function portuguese_pack4(){
+        {
+          
+       
+            var titulo = document.querySelector('#titulo')
+            titulo.textContent = 'Jantar Ultra Premium - Espaço Trapiche'
+        
+            var packs = document.querySelector('#packs')
+            packs.textContent = 'Tour'
+        
+            var info = document.querySelector('#descripcion')
+            info.textContent = 'Descubra a essência da alta gastronomia em Mendoza com nosso exclusivo Jantar Ultra Premium no Trapiche. Desfrute de um jantar de três etapas cuidadosamente preparado para deliciar seu paladar, cada prato harmonizado com vinhos selecionados da vinícola. Tudo isso com o conforto de nosso transporte privado incluído. Mergulhe em uma experiência culinária inesquecível na prestigiada Bodega Trapiche e deixe-se encantar pelos sabores e aromas de Mendoza.'
+
+            var incluye = document.querySelector('#incluye')
+            incluye.textContent = 'O que o serviço inclui'
+        
+            var item1 = document.querySelector('#item1')
+            item1.textContent = '- Degustação de vinhos ultra premium'
+        
+            var item2 = document.querySelector('#item2')
+            item2.textContent = '- Transporte privado com ar condicionado'
+
+            var item3 = document.querySelector('#item3')
+            item3.textContent = '- Jantar ultra premium de 3 etapas com harmonização'
+
+            var item4 = document.querySelector('#item4')
+            item4.textContent = '- Garrafas de água'
+/*
+            var item5 = document.querySelector('#item5')
+            item5.textContent = '- Garrafas de água'
+
+            var item6 = document.querySelector('#item6')
+            item6.textContent = '- Opções veganas e celíacas'
+        
+            var item7 = document.querySelector('#item7')
+            item7.textContent = '- Garrafas de água'
+    */
+        }
+    }
